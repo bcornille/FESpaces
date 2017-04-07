a5b0a8c5a3b5753f3980715a1ec503cfc20ce52f
Brian Cornille <bcornille7@gmail.com>
2017-04-07 10:49:52 -0500

    Added L2 mass and mixed "gradient" operators.

--- src/Integrators.hpp
+++ src/Integrators.hpp
@@ -13,7 +13,10 @@ class Integrator1D
 	public:
 		Integrator1D(json params);
 		~Integrator1D() = default;
-		MatrixXd mass(H1_1D u, H1_1D v);
+		MatrixXd mass(H1_1D u, H1_1D v, Trasform1D_Linear t);
+		MatrixXd mass(L2_1D u, L2_1D v, Trasform1D_Linear t);
+		MatrixXd grad(H1_1D u, L2_1D v, Trasform1D_Linear t);
+		MatrixXd grad(L2_1D u, H1_1D v, Trasform1D_Linear t);
 	private:
 		GaussLegendre gl;
 };
@@ -30,6 +33,48 @@ inline MatrixXd Integrator1D::mass(H1_1D u, H1_1D v, Trasform1D_Linear t)
 		VectorXd u_vals = u.eval(x_hat);
 		VectorXd v_vals = v.eval(x_hat);
 		matrix += v_vals*u_vals.traspose()*t.jacobian();
+	}
+	return matrix;
+}
+
+inline MatrixXd Integrator1D::mass(L2_1D u, L2_1D v, Trasform1D_Linear t)
+{
+	MatrixXd matrix(v.dofs(), u.dofs());
+	matrix.setZero();
+	for (int i = 0; i < gl.getN(); ++i)
+	{
+		double x_hat = gl.getNode(i);
+		VectorXd u_vals = u.eval(x_hat);
+		VectorXd v_vals = v.eval(x_hat);
+		matrix += v_vals*u_vals.traspose()*t.jacobian();
+	}
+	return matrix;
+}
+
+inline MatrixXd Integrator1D::grad(H1_1D u, L2_1D v, Trasform1D_Linear t)
+{
+	MatrixXd matrix(v.dofs(), u.dofs());
+	matrix.setZero();
+	for (int i = 0; i < gl.getN(); ++i)
+	{
+		double x_hat = gl.getNode(i);
+		VectorXd u_vals = u.evalD(x_hat);
+		VectorXd v_vals = v.eval(x_hat);
+		matrix += v_vals*u_vals.traspose();
+	}
+	return matrix;
+}
+
+inline MatrixXd Integrator1D::grad(L2_1D u, H1_1D v, Trasform1D_Linear t)
+{
+	MatrixXd matrix(v.dofs(), u.dofs());
+	matrix.setZero();
+	for (int i = 0; i < gl.getN(); ++i)
+	{
+		double x_hat = gl.getNode(i);
+		VectorXd u_vals = u.eval(x_hat);
+		VectorXd v_vals = v.evalD(x_hat);
+		matrix += v_vals*u_vals.traspose();
 	}
 	return matrix;
 }
