b179a920f38ed1c8f9f62337b6406d66d2422a51
Brian Cornille <bcornille7@gmail.com>
2017-04-24 15:17:05 -0500

    It works, but should not be pulled into master.

--- src/Poisson1D.cpp
+++ src/Poisson1D.cpp
@@ -26,10 +26,11 @@ int main(int argc, char const *argv[])
 
 	Mesh1D mesh(input["Mesh"]);
 	Integrator1D integrate(input["Integrator"]);
-	int order = (int)input["Order"];
+	int h1_order = (int)input["H1_Order"];
+	int l2_order = (int)input["L2_Order"];
 	int N_el = (int)input["Mesh"]["N"] + 1;
-	H1_1D h1(order);
-	L2_1D l2(order);
+	H1_1D h1(h1_order);
+	L2_1D l2(l2_order);
 	std::shared_ptr<Force1D> force;
 	SparseMatrix<double> system;
 	VectorXd rhs;
@@ -55,17 +56,17 @@ int main(int argc, char const *argv[])
 	if (formulation == "Standard")
 	{
 		// This is for the H1 standard system.
-		system.resize(N_el*order - 1, N_el*order - 1);
-		rhs.resize(N_el*order - 1);
-		system.reserve(VectorXi::Constant(N_el*order - 1, 2*order + 1));
+		system.resize(N_el*h1_order - 1, N_el*h1_order - 1);
+		rhs.resize(N_el*h1_order - 1);
+		system.reserve(VectorXi::Constant(N_el*h1_order - 1, 2*h1_order + 1));
 		system.setZero();
 		rhs.setZero();
 		MatrixXd minimatrix = integrate.laplace(h1, h1, mesh.getTransform(0));
 		VectorXd minirhs = integrate.force(force, h1, mesh.getTransform(0));
-		for (int j = 1; j <= order; ++j)
+		for (int j = 1; j <= h1_order; ++j)
 		{
 			rhs[j-1] += minirhs[j];
-			for (int i = 1; i <= order; ++i)
+			for (int i = 1; i <= h1_order; ++i)
 			{
 				system.coeffRef(i-1, j-1) += minimatrix(i, j);
 			}
@@ -74,23 +75,24 @@ int main(int argc, char const *argv[])
 		{
 			minimatrix = integrate.laplace(h1, h1, mesh.getTransform(n));
 			minirhs = integrate.force(force, h1, mesh.getTransform(n));
-			for (int j = 0; j <= order; ++j)
+			for (int j = 0; j <= h1_order; ++j)
 			{
-				rhs[n*order+j-1] += minirhs[j];
-				for (int i = 0; i <= order; ++i)
+				rhs[n*h1_order+j-1] += minirhs[j];
+				for (int i = 0; i <= h1_order; ++i)
 				{
-					system.coeffRef(n*order+i-1, n*order+j-1) += minimatrix(i, j);
+					system.coeffRef(n*h1_order+i-1, n*h1_order+j-1) += minimatrix(i, j);
 				}
 			}
 		}
 		minimatrix = integrate.laplace(h1, h1, mesh.getTransform(N_el - 1));
 		minirhs = integrate.force(force, h1, mesh.getTransform(N_el - 1));
-		for (int j = 0; j < order; ++j)
+		for (int j = 0; j < h1_order; ++j)
 		{
-			rhs[(N_el-1)*order+j-1] += minirhs[j];
-			for (int i = 0; i < order; ++i)
+			rhs[(N_el-1)*h1_order+j-1] += minirhs[j];
+			for (int i = 0; i < h1_order; ++i)
 			{
-				system.coeffRef((N_el-1)*order+i-1, (N_el-1)*order+j-1) += minimatrix(i, j);
+				system.coeffRef((N_el-1)*h1_order+i-1,
+					(N_el-1)*h1_order+j-1) += minimatrix(i, j);
 			}
 		}
 	}
@@ -106,33 +108,35 @@ int main(int argc, char const *argv[])
 		 * 		-<u,v> + <p,dv/dx> = 0	for all v in H^1
 		 * 		<du/dx,q> = <f,q>		for all q in L^2
 		 */
-		system.resize(2*N_el*order + 1, 2*N_el*order + 1);
-		rhs.resize(2*N_el*order + 1);
-		system.reserve(VectorXi::Constant(2*N_el*order + 1, 4*order + 1));
+		system.resize(N_el*(h1_order + l2_order) + 1, N_el*(h1_order + l2_order) + 1);
+		rhs.resize(N_el*(h1_order + l2_order) + 1);
+		system.reserve(VectorXi::Constant(N_el*(h1_order + l2_order) + 1, 4*h1_order + 1));
 		system.setZero();
 		rhs.setZero();
 		for (int n = 0; n < N_el; ++n)
 		{
 			MatrixXd mini_h1_mass = integrate.mass(h1, h1, mesh.getTransform(n));
 			MatrixXd mini_div = integrate.grad(h1, l2, mesh.getTransform(n));
-			for (int j = 0; j <= order; ++j)
+			for (int j = 0; j <= h1_order; ++j)
 			{
-				for (int i = 0; i <= order; ++i)
+				for (int i = 0; i <= h1_order; ++i)
 				{
-					system.coeffRef(n*order+i, n*order+j) -= mini_h1_mass(i, j);
+					system.coeffRef(n*h1_order+i, n*h1_order+j) -= mini_h1_mass(i, j);
 				}
-				for (int i = 0; i < order; ++i)
+				for (int i = 0; i < l2_order; ++i)
 				{
-					system.coeffRef((N_el+n)*order+i+1, n*order+j) += mini_div(i, j);
+					system.coeffRef(N_el*h1_order+n*l2_order+i+1,
+						n*h1_order+j) += mini_div(i, j);
 				}
 			}
 			VectorXd minirhs = integrate.force(force, l2, mesh.getTransform(n));
-			for (int i = 0; i < order; ++i)
+			for (int i = 0; i < l2_order; ++i)
 			{
-				rhs[(N_el+n)*order+i+1] = minirhs[i];
+				rhs[N_el*h1_order+n*l2_order+i+1] = minirhs[i];
 			}
 		}
-		SparseMatrix<double> identity(2*N_el*order + 1, 2*N_el*order + 1);
+		SparseMatrix<double> identity(N_el*(h1_order + l2_order) + 1,
+			N_el*(h1_order + l2_order) + 1);
 		identity.setIdentity();
 		system = system.selfadjointView<Lower>()*identity;
 	}
@@ -148,56 +152,60 @@ int main(int argc, char const *argv[])
 		 * 		<u,v> + <dp/dx,v> = 0	for all v in L^2
 		 * 		<u,dq/dx> = -<f,q>		for all q in H^1
 		 */
-		system.resize(2*N_el*order - 1, 2*N_el*order - 1);
-		rhs.resize(2*N_el*order - 1);
-		system.reserve(VectorXi::Constant(2*N_el*order - 1, 3*order));
+		system.resize(N_el*(h1_order + l2_order) - 1, N_el*(h1_order + l2_order) - 1);
+		rhs.resize(N_el*(h1_order + l2_order) - 1);
+		system.reserve(VectorXi::Constant(N_el*(h1_order + l2_order) - 1,
+			l2_order + 2*h1_order));
 		system.setZero();
 		rhs.setZero();
 		for (int n = 0; n < N_el; ++n)
 		{
 			MatrixXd mini_l2_mass = integrate.mass(l2, l2, mesh.getTransform(n));
-			for (int j = 0; j < order; ++j)
+			for (int j = 0; j < l2_order; ++j)
 			{
-				for (int i = 0; i < order; ++i)
+				for (int i = 0; i < l2_order; ++i)
 				{
-					system.coeffRef(n*order+i, n*order+j) += mini_l2_mass(i, j);
+					system.coeffRef(n*l2_order+i, n*l2_order+j) += mini_l2_mass(i, j);
 				}
 			}
 		}
 		MatrixXd mini_grad = integrate.grad(h1, l2, mesh.getTransform(0));
 		VectorXd minirhs = integrate.force(force, h1, mesh.getTransform(0));
-		for (int j = 1; j <= order; ++j)
+		for (int j = 1; j <= h1_order; ++j)
 		{
-			rhs[N_el*order+j-1] -= minirhs[j];
-			for (int i = 0; i < order; ++i)
+			rhs[N_el*l2_order+j-1] -= minirhs[j];
+			for (int i = 0; i < l2_order; ++i)
 			{
-				system.coeffRef(i, N_el*order+j-1) += mini_grad(i, j);
+				system.coeffRef(i, N_el*l2_order+j-1) += mini_grad(i, j);
 			}
 		}
 		for (int n = 1; n < N_el - 1; ++n)
 		{
 			mini_grad = integrate.grad(h1, l2, mesh.getTransform(n));
 			minirhs = integrate.force(force, h1, mesh.getTransform(n));
-			for (int j = 0; j <= order; ++j)
+			for (int j = 0; j <= h1_order; ++j)
 			{
-				rhs[(N_el+n)*order+j-1] -= minirhs[j];
-				for (int i = 0; i < order; ++i)
+				rhs[N_el*l2_order+n*h1_order+j-1] -= minirhs[j];
+				for (int i = 0; i < l2_order; ++i)
 				{
-					system.coeffRef(n*order+i, (N_el+n)*order+j-1) += mini_grad(i, j);
+					system.coeffRef(n*l2_order+i,
+						N_el*l2_order+n*h1_order+j-1) += mini_grad(i, j);
 				}
 			}
 		}
 		mini_grad = integrate.grad(h1, l2, mesh.getTransform(N_el - 1));
 		minirhs = integrate.force(force, h1, mesh.getTransform(N_el - 1));
-		for (int j = 0; j < order; ++j)
+		for (int j = 0; j < h1_order; ++j)
 		{
-			rhs[(2*N_el-1)*order+j-1] -= minirhs[j];
-			for (int i = 0; i < order; ++i)
+			rhs[N_el*l2_order+(N_el-1)*h1_order+j-1] -= minirhs[j];
+			for (int i = 0; i < l2_order; ++i)
 			{
-				system.coeffRef((N_el-1)*order+i, (2*N_el-1)*order+j-1) += mini_grad(i, j);
+				system.coeffRef((N_el-1)*l2_order+i,
+					N_el*l2_order+(N_el-1)*h1_order+j-1) += mini_grad(i, j);
 			}
 		}
-		SparseMatrix<double> identity(2*N_el*order - 1, 2*N_el*order - 1);
+		SparseMatrix<double> identity(N_el*(h1_order + l2_order) - 1,
+			N_el*(h1_order + l2_order) - 1);
 		identity.setIdentity();
 		system = system.selfadjointView<Upper>()*identity;
 	}
@@ -225,37 +233,40 @@ int main(int argc, char const *argv[])
 	double error = 0.0;
 	if (formulation == "Standard")
 	{
-		VectorXd segment(order + 1);
+		VectorXd segment(h1_order + 1);
 		segment[0] = 0.0;
-		segment.tail(order) = x.segment(0, order);
+		segment.tail(h1_order) = x.segment(0, h1_order);
 		error += integrate.error(force, h1, segment, mesh.getTransform(0));
 		for (int n = 1; n < N_el - 1; ++n)
 		{
-			error += integrate.error(force, h1, x.segment(n*order - 1, order + 1), mesh.getTransform(n));
+			error += integrate.error(force, h1,
+				x.segment(n*h1_order - 1, h1_order + 1), mesh.getTransform(n));
 		}
-		segment.head(order) = x.segment((N_el-1)*order-1, order);
-		segment[order] = 0.0;
+		segment.head(h1_order) = x.segment((N_el-1)*h1_order-1, h1_order);
+		segment[h1_order] = 0.0;
 		error += integrate.error(force, h1, segment, mesh.getTransform(N_el-1));
 	}
 	else if(formulation == "Mixed")
 	{
 		for (int n = 0; n < N_el; ++n)
 		{
-			error += integrate.error(force, l2, x.segment((N_el+n)*order+1, order), mesh.getTransform(n));
+			error += integrate.error(force, l2,
+				x.segment(N_el*h1_order+n*l2_order+1, l2_order), mesh.getTransform(n));
 		}
 	}
 	else if (formulation == "Mimetic")
 	{
-		VectorXd segment(order + 1);
+		VectorXd segment(h1_order + 1);
 		segment[0] = 0.0;
-		segment.tail(order) = x.segment(N_el*order, order);
+		segment.tail(h1_order) = x.segment(N_el*l2_order, h1_order);
 		error += integrate.error(force, h1, segment, mesh.getTransform(0));
 		for (int n = 1; n < N_el - 1; ++n)
 		{
-			error += integrate.error(force, h1, x.segment((N_el+n)*order-1, order + 1), mesh.getTransform(n));
+			error += integrate.error(force, h1,
+				x.segment(N_el*l2_order+n*h1_order-1, h1_order + 1), mesh.getTransform(n));
 		}
-		segment.head(order) = x.segment((2*N_el-1)*order-1, order);
-		segment[order] = 0.0;
+		segment.head(h1_order) = x.segment(N_el*l2_order+(N_el-1)*h1_order-1, h1_order);
+		segment[h1_order] = 0.0;
 		error += integrate.error(force, h1, segment, mesh.getTransform(N_el-1));
 	}
 
